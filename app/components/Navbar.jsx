a66f3a7de826897249e382faf1ded942707eaa6d
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-10-13 09:08:54 +0800

    added navbar logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 export default function Navbar() {
   return (
     <nav className="p-4 bg-green-500">
+      <Image
+        src={Logo}
+        alt="Dojo helpdesk logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Dojo</h1>
       <ul className="flex align-center justify-center">
         <li>
           <Link href="/" className=" text-white">
