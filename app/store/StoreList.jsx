8ea8f46e58b74f57968f2172b9c2d5923d88a002
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-10-16 09:57:26 +0800

    modify next.config and remove card margin top and bottom

--- app/store/StoreList.jsx
+++ app/store/StoreList.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const getData = async () => {
   const response = await fetch("https://fakestoreapi.com/products", {
     next: {
-      revalidate: 0,
+      revalidate: 10,
     },
   });
 
@@ -18,9 +19,13 @@ export default async function StoreList() {
       {data.map((d, index) => {
         const { id, title, image } = d;
         return (
-          <div key={index}>
-            <Link href={`/store/${id}`}>
-              <img src={image} alt={title} />
+          <div className="card my-0 hover:bg-sky-700" key={index}>
+            <Link
+              href={`/store/${id}`}
+              className="flex items-center justify-center h-full"
+            >
+              <Image src={image} alt={title} width={100} height={100} />
+              {/* <img /> */}
             </Link>
           </div>
         );
