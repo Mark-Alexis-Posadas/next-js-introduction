d485b389d27104f17ae6a9d4ed21a646c1f4733e
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-11-04 10:14:32 +0800

    add navbar margin left

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,23 +15,23 @@ export default function Navbar() {
       />
       <h1 className="lg:me-auto">Dojo</h1>
       <ul className="w-full sm:w-60 lg:w-auto flex align-center justify-center flex-col sm:flex-row">
-        <li className="text-center mb-3 sm:mb-0">
+        <li className="text-center ml-4 mb-3 sm:mb-0">
           <Link href="/" className=" text-white">
             Dashboard
           </Link>
         </li>
-        <li className="mx-5 text-center mb-3 sm:mb-0">
+        <li className="text-center ml-4 mb-3 sm:mb-0">
           <Link href="/tickets" className=" text-white">
             Tickets
           </Link>
         </li>
-        <li className="text-center">
+        <li className="text-center ml-4">
           <Link href="/store" className="text-white">
             Store
           </Link>
         </li>
 
-        <li className="text-center">
+        <li className="text-center ml-4">
           <Link href="/test" className="text-white">
             Test Components
           </Link>
